ebc558e3bf71f79184c06428b0f67f4314044e77
Sarah Veness <sarahhveness@gmail.com>
2017-06-21 15:29:45 -0700

    initial commit

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key: 'pJQy6oXwEpkJtbjZYEGxHO7Dp',
-  consumer_secret: 'Ce8uWE24AJcetO9xbyYRrMDHsooWc9YbxU5995aKSkZqgydXU5',
-  access_token_key: '21154624-1cJStm4crjGRkwuQR3OKAZLOnUwsNvwEO1820mbjN',
-  access_token_secret: 'yxIvkXc07hUImMiOoYmluaW26ZW2tSlKBusFPmI9m4KzE'
-}
